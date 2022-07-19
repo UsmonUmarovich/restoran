b98bb81e5c73cd0c16dd832ce1a3ba0cbd69afe5
verve-neowise <verve.neowise@gmail.com>
2022-07-19 13:56:58 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import { engine } from 'express-handlebars'
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('.hbs', engine({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.set('views', './view')
+
+
+app.listen(8080, console.log('Server started on http://localhost:8080'))
