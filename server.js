4e43f7887e0ad017d8eaed2760c5b2e8cc0453bb
UsmonUmarovich <usmon.umarovich.09@gmail.com>
2022-07-19 14:52:29 +0500

    server.js update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import { engine } from "express-handlebars"
+import registerRouters from "./routers/register.routers.js"
 
 const app = express()
 
@@ -11,6 +12,8 @@ app.use(express.urlencoded({ extended: true }))
 app.engine(".hbs", engine({ extname: ".hbs" }))
 app.set("view engine", ".hbs")
 app.set("views", "./views")
+
+app.use(registerRouters)
 
 app.listen(8080, (req, res) => {
     console.log("Server is running on http://localhost:8080")
